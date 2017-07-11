7b03d7f3e3d0915fdebd59209a4d19ee64a9d4e1
kenzocosmos <kenzocosmos@gmail.com>
2017-07-11 17:18:26 +0000

    Dev tool initial commit

---
+++ lib/Goodnow_view_tool.rb
@@ -0,0 +1,6 @@
+require "Goodnow_view_tool/version"
+require "Goodnow_view_tool/renderer"
+
+module GoodnowViewTool
+  # Your code goes here...
+end
---
+++ lib/Goodnow_view_tool/version.rb
@@ -0,0 +1,3 @@
+module GoodnowViewTool
+  VERSION = "0.1.0"
+end
